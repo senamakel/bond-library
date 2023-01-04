c711028ec03256bdad2d0904403ce82d49809b8f
spaceturtleship <spaceturtleship@protonmail.com>
2023-01-04 10:08:21 +0000

    Updates to sync with data-library

--- src/tokens/frax.ts
+++ src/tokens/frax.ts
@@ -4,12 +4,13 @@ export default {
   symbol: "FRAX",
   logoUrl: "https://storageapi.fleek.co/fc635ae1-c8aa-4181-b7db-801a533b8fa9-bucket/FRAX.png",
   priceSources: [{ source: "coingecko", apiId: "frax" }],
-  purchaseLinks: { 
-  [CHAIN_ID.ETHEREUM_MAINNET]: "https://curve.fi/",
-  [CHAIN_ID.ARBITRUM_MAINNET]: "https://saddle.exchange/#/",
-}, 
-  addresses: { 
-  [CHAIN_ID.ETHEREUM_MAINNET]: "0x853d955acef822db058eb8505911ed77f175b99e" ,
-  [CHAIN_ID.ARBITRUM_MAINNET]: "0x17FC002b466eEc40DaE837Fc4bE5c67993ddBd6F",
-},
+  purchaseLinks: {
+    [CHAIN_ID.ETHEREUM_MAINNET]: "https://curve.fi/",
+    [CHAIN_ID.ARBITRUM_MAINNET]: "https://saddle.exchange/#/",
+  },
+  addresses: {
+    [CHAIN_ID.ETHEREUM_MAINNET]: "0x853d955acef822db058eb8505911ed77f175b99e" ,
+    [CHAIN_ID.ARBITRUM_MAINNET]: "0x17FC002b466eEc40DaE837Fc4bE5c67993ddBd6F",
+    [CHAIN_ID.GOERLI_TESTNET]: "0x73651AD693531F9937528009cC204a4d9b696a68",
+  },
 };
--- src/tokens/usdc.ts
+++ src/tokens/usdc.ts
@@ -2,16 +2,19 @@ import { CUSTOM_PRICE_FEEDS, CHAIN_ID } from "../constants";
 export default {
   name: "USD Coin",
   symbol: "USDC",
-  logoUrl: "https://storageapi.fleek.co/fc635ae1-c8aa-4181-b7db-801a533b8fa9-bucket/USDC.png",
+  logoUrl:
+    "https://storageapi.fleek.co/fc635ae1-c8aa-4181-b7db-801a533b8fa9-bucket/USDC.png",
   addresses: {
     [CHAIN_ID.ETHEREUM_MAINNET]: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
-    [CHAIN_ID.GOERLI_TESTNET]: "0xD87Ba7A50B2E7E660f678A895E4B72E7CB4CCd9C",
+    [CHAIN_ID.GOERLI_TESTNET]: [
+      "0xD87Ba7A50B2E7E660f678A895E4B72E7CB4CCd9C",
+      "0xe0c9275e44ea80ef17579d33c55136b7da269aeb"
+    ],
     [CHAIN_ID.ARBITRUM_MAINNET]: "0xff970a61a04b1ca14834a43f5de4533ebddb5cc8",
     [CHAIN_ID.ARBITRUM_GOERLI_TESTNET]: "0xD87Ba7A50B2E7E660f678A895E4B72E7CB4CCd9C",
   },
   purchaseLinks: {
     [CHAIN_ID.ETHEREUM_MAINNET]: "https://curve.fi/",
-    [CHAIN_ID.ARBITRUM_MAINNET]: "https://app.sushi.com/swap",
     [CHAIN_ID.ARBITRUM_MAINNET]: "https://saddle.exchange/#/",
     [CHAIN_ID.GOERLI_TESTNET]: "https://app.compound.finance/",
   },
