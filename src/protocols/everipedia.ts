a3ac392110bb2b898991ab62ff1ba355c86cb2f2
Adebayo Olamilekan <75235148+OleanjiKingCode@users.noreply.github.com>
2023-01-09 19:26:35 +0100

    Added everipedia to bond library (#72)

---
+++ src/protocols/everipedia.ts
@@ -0,0 +1,37 @@
+import { CHAIN_ID, CUSTOM_PRICE_FEEDS } from "../constants";
+import { ProtocolDefinition } from "../public-types";
+
+/* Protocol Template */
+export default {
+  name: "IQ",
+  description: "The World's Largest Blockchain & Crypto Encyclopedia",
+  logoUrl: "https://your-dao.defi/your-protocol-logo.png",
+
+  links: {
+    governanceVote: "https://snapshot.everipedia.com/",
+    homepage: "https://iq.wiki/",
+    twitter: "https://twitter.com/IQWIKI",
+    github: "https://github.com/EveripediaNetwork",
+    discord: "https://discord.gg/x9EWvTcPXt",
+    telegram: "https://t.me/everipedia",
+    staking: "https://iq.braindao.org/dashboard/stake",
+  },
+  issuerAddresses: {
+    [CHAIN_ID.ETHEREUM_MAINNET]: ["0x8aCc50345921b4A635705Ec7f3AC042B3407BD58"],
+  },
+  tokens: [
+    {
+      name: "IQ",
+      symbol: "IQ",
+      logoUrl: "https://iq.wiki/branding/downloadassets/logooriginalbrain.png",
+      addresses: {
+        [CHAIN_ID.ETHEREUM_MAINNET]: "0x579cea1889991f68acc35ff5c3dd0621ff29b0c9",
+      },
+      purchaseLinks: {
+        [CHAIN_ID.ETHEREUM_MAINNET]:
+          "https://app.frax.finance/swap/main?chain=ethereum&from=0x579CEa1889991f68aCc35Ff5c3dd0621fF29b0C9&to=0x853d955aCEf822Db058eb8505911ED77F175b99e",
+      },
+      priceSources: [{ source: "coingecko", apiId: "everipedia" }],
+    },
+  ],
+} as ProtocolDefinition;
