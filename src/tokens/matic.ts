ad95c5e543adbb37b62fe4742a39782e8b404019
spaceturtleship <spaceturtleship@protonmail.com>
2023-01-12 12:29:03 +0000

    Add Matic and Polygon Mumbai address for USDC

---
+++ src/tokens/matic.ts
@@ -0,0 +1,12 @@
+import { CHAIN_ID } from "../constants";
+
+export default {
+  name: "Matic",
+  symbol: "MATIC",
+  logoUrl: "https://storageapi.fleek.co/6e41a561-e275-4698-bc36-548d30a80e96-bucket/POLYGON.svg",
+  priceSources: [{ source: "coingecko", apiId: "matic-network" }],
+  purchaseLinks: {},
+  addresses: {
+    matic: "",
+  },
+};
