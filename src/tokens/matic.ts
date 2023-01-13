7b5100898fc8134b364c97d070c9323d94a97536
spaceturtleship <spaceturtleship@protonmail.com>
2023-01-13 10:24:08 +0000

    Add WMATIC polygon mumbai

--- src/tokens/matic.ts
+++ src/tokens/matic.ts
@@ -1,8 +1,8 @@
 import { CHAIN_ID } from "../constants";
 
 export default {
-  name: "Matic",
-  symbol: "MATIC",
+  name: "Wrapped Matic",
+  symbol: "WMATIC",
   logoUrl: "https://storageapi.fleek.co/6e41a561-e275-4698-bc36-548d30a80e96-bucket/POLYGON.svg",
   priceSources: [{ source: "coingecko", apiId: "matic-network" }],
   purchaseLinks: {},
@@ -10,5 +10,6 @@ export default {
     matic: "",
     [CHAIN_ID.POLYGON_MAINNET]: "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270",
     [CHAIN_ID.FANTOM_MAINNET]: "0x40df1ae6074c35047bff66675488aa2f9f6384f3",
+    [CHAIN_ID.POLYGON_MUMBAI_TESTNET]: "0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889",
   },
 };
