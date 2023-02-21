1d5a44464ebfb5155027f989141059c5e599374e
spaceturtleship <spaceturtleship@protonmail.com>
2023-02-21 16:57:01 +0900

    Fix BFR api-id

--- src/protocols/buffer.ts
+++ src/protocols/buffer.ts
@@ -4,7 +4,8 @@ import { ProtocolDefinition } from "../public-types";
 /* Protocol Template */
 export default {
   name: "Buffer Finance", // Your protocol's display name, formatted as it will appear in the BondProtocol dApp
-  description: "Buffer Finance is a non-custodial, exotic options trading platform built to trade short-term price volatility and hedge risk of high-leverage positions.", // A short protocol description
+  description:
+    "Buffer Finance is a non-custodial, exotic options trading platform built to trade short-term price volatility and hedge risk of high-leverage positions.", // A short protocol description
   logoUrl: "https://github.com/Buffer-Finance/Buffer-Media/blob/main/new_logo_50_50.png?raw=true",
   // Links to landing page, socials and docs
   links: {
@@ -24,7 +25,7 @@ export default {
   */
   issuerAddresses: {
     [CHAIN_ID.ARBITRUM_MAINNET]: ["0xfa1e2DD94D6665bb964192Debac09c16242f8a48"],
-    [CHAIN_ID.ARBITRUM_GOERLI_TESTNET]: ["0xFbEA9559AE33214a080c03c68EcF1D3AF0f58A7D"]
+    [CHAIN_ID.ARBITRUM_GOERLI_TESTNET]: ["0xFbEA9559AE33214a080c03c68EcF1D3AF0f58A7D"],
   },
   /*
    Tokens specific to your protocol like your governance token or LPs can be added here.
@@ -40,7 +41,7 @@ export default {
       // Token contract addresses
       addresses: {
         [CHAIN_ID.ARBITRUM_MAINNET]: "0x1A5B0aaF478bf1FDA7b934c76E7692D722982a6D",
-        [CHAIN_ID.ARBITRUM_GOERLI_TESTNET]: "0x89fEF05446aEA764C53a2f09bB763876FB57ea8E"
+        [CHAIN_ID.ARBITRUM_GOERLI_TESTNET]: "0x89fEF05446aEA764C53a2f09bB763876FB57ea8E",
       },
       purchaseLinks: { [CHAIN_ID.ARBITRUM_MAINNET]: "https://app.openocean.finance/CLASSIC#/ARBITRUM/USDC/BFR" }, // Where to acquire your token
       priceSources: [
@@ -49,7 +50,7 @@ export default {
            Although custom price feeds are supported, some features such as bond price history charts
            do not currently support custom price feeds.
         */
-        { source: "coingecko", apiId: "buffer-token" },
+        { source: "coingecko", apiId: "ibuffer-token" },
         /*
           If you need to add a custom price function please add it in ../custom-price-feeds.ts
           then uncomment the following line and refer to it using the name you set in the
@@ -57,6 +58,6 @@ export default {
         */
         // { source: "custom", customPriceFunction: CUSTOM_PRICE_FEEDS.US_STABLE },
       ],
-    }
+    },
   ],
 } as ProtocolDefinition;
